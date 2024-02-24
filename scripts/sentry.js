93f30ae4e7c273cf7923cea32c0a5ff8c08cfc59
Gecko! <74975165+SmartGecko44@users.noreply.github.com>
2024-02-24 01:42:27 +0100

    test v3

--- scripts/sentry.js
+++ scripts/sentry.js
@@ -3,7 +3,11 @@ import * as Sentry from '@sentry/browser';
 window.sentryOnLoad = function () {
     Sentry.init({
         dsn: 'https://028f532367f52ee276cc72cc87c08951@o4506786880028672.ingest.sentry.io/4506786933899264',
-        integrations: [Sentry.browserTracingIntegration()],
+
+        integrations: [
+            Sentry.browserTracingIntegration(),
+            Sentry.replayIntegration(),
+        ],
         tracePropagationTargets: ['https://gxcko.me']
     });
 };
