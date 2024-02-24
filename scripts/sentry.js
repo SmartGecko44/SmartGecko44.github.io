63d7b8da379a053d874d922df0e915a3099eceda
Gecko! <74975165+SmartGecko44@users.noreply.github.com>
2024-02-24 01:26:04 +0100

    Fix sentry.js

--- scripts/sentry.js
+++ scripts/sentry.js
@@ -4,6 +4,6 @@ window.sentryOnLoad = function () {
     Sentry.init({
         dsn: 'https://028f532367f52ee276cc72cc87c08951@o4506786880028672.ingest.sentry.io/4506786933899264',
         integrations: [Sentry.browserTracingIntegration()],
-        tracePropagationTargets: ['https://gxcko.me', /\/api\//]
+        tracePropagationTargets: ['https://gxcko.me']
     });
 };
