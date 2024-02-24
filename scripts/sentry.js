36d197e2cb9d7ca2f05c37602b6b0b32a13527ec
Gecko! <74975165+SmartGecko44@users.noreply.github.com>
2024-02-24 13:24:53 +0100

    Fixed sentry

--- scripts/sentry.js
+++ scripts/sentry.js
@@ -1,12 +1,12 @@
 import * as Sentry from '@sentry/browser';
 
 window.sentryOnLoad = function () {
+    console.log('sentryOnLoad');
     Sentry.init({
         dsn: 'https://028f532367f52ee276cc72cc87c08951@o4506786880028672.ingest.sentry.io/4506786933899264',
 
         integrations: [
             Sentry.browserTracingIntegration(),
-            Sentry.replayIntegration(),
         ],
         tracePropagationTargets: ['https://gxcko.me']
     });
