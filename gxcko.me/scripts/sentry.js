837274e24d34a8d4a78ca2bed5d3019d38585f78
Gecko! <74975165+SmartGecko44@users.noreply.github.com>
2024-08-09 23:44:18 +0200

    Check sentry

--- gxcko.me/scripts/sentry.js
+++ gxcko.me/scripts/sentry.js
@@ -2,7 +2,6 @@ import { init, browserTracingIntegration } from "@sentry/browser";
 
 
 window.sentryOnLoad = function () {
-    console.error('Sentry loaded');
     init({
         dsn: 'https://028f532367f52ee276cc72cc87c08951@o4506786880028672.ingest.sentry.io/4506786933899264',
 
